b2ef688306862957bf0f234c5596e04910c06292
malamutes <hehexdroflmfao@gmail.com>
2024-11-03 21:38:54 +0800

    NeedOptimizeNext

--- src/Client/Game/Game.tsx
+++ src/Client/Game/Game.tsx
@@ -3,72 +3,246 @@ import './Game.css'
 import { useEffect, useState } from "react";
 
 export default function Games() {
-    const [hoverDuration, setHoverDuration] = useState<number[]>([0, 0, 0, 0, 0]);
-    const [timers, setTimers] = useState<Array<number | null>>(Array(5).fill(null));
-    const [timersDrop, setTimersDrop] = useState<Array<number | null>>(Array(5).fill(null));
-
-
+    const [hoverDurationOne, setHoverDurationOne] = useState<number>(0);
+    const [timerOne, setTimerOne] = useState<number | null>(null);
+    const [timerDropOne, setTimerDropOne] = useState<number | null>(null);
 
     //0 and 150 hover dur
-    function startTimer(index: number) {
-        const newTimersDrop = [...timersDrop];
-        const newTimers = [...timers];
+    function startTimerOne() {
+        if (hoverDurationOne < 150) {
+            if (timerDropOne) {
+                clearInterval(timerDropOne);
+                setTimerDropOne(null);
+            }
 
-        if (newTimersDrop[index]) {
-            clearInterval(newTimersDrop[index]);
-            newTimersDrop[index] = null;
+            const newTimer = setInterval(() => {
+                setHoverDurationOne(hoverDurationOne => (Math.min(hoverDurationOne + 1, 150)));
+            }, 10); // 10ms
+
+            setTimerOne(newTimer)
         }
 
-        setTimersDrop(newTimersDrop);
-
-        const newTimer = setInterval(() => {
-            setHoverDuration(prevHoverDuration => {
-                const newDuration = Math.min(prevHoverDuration[index] + 1, 150);
-                return [...prevHoverDuration.slice(0, index), newDuration, ...prevHoverDuration.slice(index + 1)];
-            });
-        }, 10); // 10ms
-
-
-        newTimers[index] = newTimer;
-        setTimers(newTimers);
     };
 
-    function stopTimer(index: number) {
-        const newTimersDrop = [...timersDrop];
-        const newTimers = [...timers];
+    function stopTimerOne() {
+        if (hoverDurationOne < 150) {
+            if (timerOne) {
+                clearInterval(timerOne);
+                setTimerOne(null);
+            }
 
-        if (newTimers[index]) {
-            clearInterval(newTimers[index]);
-            newTimers[index] = null;
+            console.log("hello1");
+
+            const newTimer = setInterval(() => {
+                setHoverDurationOne(hoverDurationOne => (Math.max(hoverDurationOne - 1, 0)));
+            }, 10); // 10ms
+
+            setTimerDropOne(newTimer);
         }
-
-        setTimers(newTimers);
-
-        const newTimer = setInterval(() => {
-            setHoverDuration(prevHoverDuration => {
-                const newDuration = Math.max(prevHoverDuration[index] - 1, 0);
-                return [...prevHoverDuration.slice(0, index), newDuration, ...prevHoverDuration.slice(index + 1)];
-            });
-        }, 10); // 10ms
-
-        newTimersDrop[index] = newTimer;
-        setTimersDrop(newTimersDrop);
 
     }
 
+
     useEffect(() => {
-        const newTimersDrop = [...timersDrop];
-        for (let i = 0; i < 5; i++) {
-            if (hoverDuration[i] <= 0 && timersDrop[i] !== null) {
-                clearInterval(timersDrop[i]!);
-                newTimersDrop[i] = null;
-                setTimersDrop(newTimersDrop);
-            }
+        if (hoverDurationOne <= 0 && timerDropOne) {
+            clearInterval(timerDropOne);
+            setTimerDropOne(null);
         }
-        console.log(hoverDuration);
-    }, [hoverDuration]);
+        else if (hoverDurationOne >= 150 && timerOne) {
+            clearInterval(timerOne);
+            setTimerOne(null);
+        }
+        console.log(hoverDurationOne);
+    }, [hoverDurationOne]);
 
+    // Timer 2
+    const [hoverDurationTwo, setHoverDurationTwo] = useState<number>(0);
+    const [timerTwo, setTimerTwo] = useState<number | null>(null);
+    const [timerDropTwo, setTimerDropTwo] = useState<number | null>(null);
 
+    function startTimerTwo() {
+        if (hoverDurationTwo < 150) {
+            if (timerDropTwo) {
+                clearInterval(timerDropTwo);
+                setTimerDropTwo(null);
+            }
+
+            const newTimer = setInterval(() => {
+                setHoverDurationTwo(hoverDurationTwo => Math.min(hoverDurationTwo + 1, 150));
+            }, 10); // 10ms
+
+            setTimerTwo(newTimer);
+        }
+    }
+
+    function stopTimerTwo() {
+        if (hoverDurationTwo < 150) {
+            if (timerTwo) {
+                clearInterval(timerTwo);
+                setTimerTwo(null);
+            }
+
+            console.log("hello2");
+
+            const newTimer = setInterval(() => {
+                setHoverDurationTwo(hoverDurationTwo => Math.max(hoverDurationTwo - 1, 0));
+            }, 10); // 10ms
+
+            setTimerDropTwo(newTimer);
+        }
+    }
+
+    useEffect(() => {
+        if (hoverDurationTwo <= 0 && timerDropTwo) {
+            clearInterval(timerDropTwo);
+            setTimerDropTwo(null);
+        }
+        else if (hoverDurationTwo >= 150 && timerTwo) {
+            clearInterval(timerTwo);
+            setTimerTwo(null);
+        }
+        console.log(hoverDurationTwo);
+    }, [hoverDurationTwo]);
+
+    // Timer 3
+    const [hoverDurationThree, setHoverDurationThree] = useState<number>(0);
+    const [timerThree, setTimerThree] = useState<number | null>(null);
+    const [timerDropThree, setTimerDropThree] = useState<number | null>(null);
+    function startTimerThree() {
+        if (hoverDurationThree < 150) {
+            if (timerDropThree) {
+                clearInterval(timerDropThree);
+                setTimerDropThree(null);
+            }
+
+            const newTimer = setInterval(() => {
+                setHoverDurationThree(hoverDurationThree => Math.min(hoverDurationThree + 1, 150));
+            }, 10); // 10ms
+
+            setTimerThree(newTimer);
+        }
+    }
+
+    function stopTimerThree() {
+        if (hoverDurationThree < 150) {
+            if (timerThree) {
+                clearInterval(timerThree);
+                setTimerThree(null);
+            }
+            console.log("hello3");
+
+            const newTimer = setInterval(() => {
+                setHoverDurationThree(hoverDurationThree => Math.max(hoverDurationThree - 1, 0));
+            }, 10); // 10ms
+
+            setTimerDropThree(newTimer);
+        }
+    }
+
+    useEffect(() => {
+        if (hoverDurationThree <= 0 && timerDropThree) {
+            clearInterval(timerDropThree);
+            setTimerDropThree(null);
+        }
+        else if (hoverDurationThree >= 150 && timerThree) {
+            clearInterval(timerThree);
+            setTimerThree(null);
+        }
+    }, [hoverDurationThree]);
+
+    // Timer 4
+    const [hoverDurationFour, setHoverDurationFour] = useState<number>(0);
+    const [timerFour, setTimerFour] = useState<number | null>(null);
+    const [timerDropFour, setTimerDropFour] = useState<number | null>(null);
+
+    function startTimerFour() {
+        if (hoverDurationFour < 150) {
+            if (timerDropFour) {
+                clearInterval(timerDropFour);
+                setTimerDropFour(null);
+            }
+
+            const newTimer = setInterval(() => {
+                setHoverDurationFour(hoverDurationFour => Math.min(hoverDurationFour + 1, 150));
+            }, 10); // 10ms
+
+            setTimerFour(newTimer);
+        }
+    }
+
+    function stopTimerFour() {
+        if (hoverDurationFour < 150) {
+            if (timerFour) {
+                clearInterval(timerFour);
+                setTimerFour(null);
+            }
+            console.log("hello4");
+
+            const newTimer = setInterval(() => {
+                setHoverDurationFour(hoverDurationFour => Math.max(hoverDurationFour - 1, 0));
+            }, 10); // 10ms
+
+            setTimerDropFour(newTimer);
+        }
+    }
+
+    useEffect(() => {
+        if (hoverDurationFour <= 0 && timerDropFour) {
+            clearInterval(timerDropFour);
+            setTimerDropFour(null);
+        }
+        else if (hoverDurationFour >= 150 && timerFour) {
+            clearInterval(timerFour);
+            setTimerFour(null);
+        }
+    }, [hoverDurationFour]);
+
+    // Timer 5
+    const [hoverDurationFive, setHoverDurationFive] = useState<number>(0);
+    const [timerFive, setTimerFive] = useState<number | null>(null);
+    const [timerDropFive, setTimerDropFive] = useState<number | null>(null);
+
+    function startTimerFive() {
+        if (hoverDurationFive < 150) {
+            if (timerDropFive) {
+                clearInterval(timerDropFive);
+                setTimerDropFive(null);
+            }
+
+            const newTimer = setInterval(() => {
+                setHoverDurationFive(hoverDurationFive => Math.min(hoverDurationFive + 1, 150));
+            }, 10); // 10ms
+
+            setTimerFive(newTimer);
+        }
+    }
+
+    function stopTimerFive() {
+        if (hoverDurationFive < 150) {
+            if (timerFive) {
+                clearInterval(timerFive);
+                setTimerFive(null);
+            }
+            console.log("hello5");
+
+            const newTimer = setInterval(() => {
+                setHoverDurationFive(hoverDurationFive => Math.max(hoverDurationFive - 1, 0));
+            }, 10); // 10ms
+
+            setTimerDropFive(newTimer);
+        }
+    }
+
+    useEffect(() => {
+        if (hoverDurationFive <= 0 && timerDropFive) {
+            clearInterval(timerDropFive);
+            setTimerDropFive(null);
+        }
+        else if (hoverDurationFive >= 150 && timerFive) {
+            clearInterval(timerFive);
+            setTimerFive(null);
+        }
+    }, [hoverDurationFive]);
 
     return (
         <>
@@ -76,58 +250,73 @@ export default function Games() {
                 <h1 style={{ margin: '75px', textAlign: 'center' }}>GAMES WHICH LORE I LIKE</h1>
                 <Row >
                     <Col>
-                        <div className={`animated-border`} onMouseEnter={() => startTimer(0)} onMouseLeave={() => stopTimer(0)}
+                        <div className={`animated-border`} onMouseEnter={() => startTimerOne()} onMouseLeave={() => stopTimerOne()}
                             style={{
-                                backgroundImage: `conic-gradient(darkgrey ${hoverDuration[0] / 1.5}%, transparent ${hoverDuration[0]}%`,
-                                cursor: `${hoverDuration[0] === 150 ? "pointer" : "progress"}`
+                                backgroundImage: `conic-gradient(darkgrey ${hoverDurationOne / 1.5}%, transparent ${hoverDurationOne}%`,
+                                cursor: `${hoverDurationOne === 150 ? "pointer" : "progress"}`
                             }}>
-                            <Image src="/Public/Images/GameImages/Bloodborne.jpg" fluid style={{ padding: '5px' }} />
+                            <Image src="/Public/Images/GameImages/Bloodborne.jpg" fluid style={{
+                                padding: '5px',
+                                transition: 'transform 1s', transform: `scale(${hoverDurationOne === 150 ? 1.1 : 1})`
+                            }} />
                         </div>
 
                     </Col>
 
                     <Col>
                         <Row >
-                            <div className={`animated-border`} onMouseEnter={() => startTimer(1)} onMouseLeave={() => stopTimer(1)}
+                            <div className={`animated-border`} onMouseEnter={() => startTimerTwo()} onMouseLeave={() => stopTimerTwo()}
                                 style={{
-                                    backgroundImage: `conic-gradient(yellow ${hoverDuration[1] / 1.5}%, transparent ${hoverDuration[1]}%`,
-                                    cursor: `${hoverDuration[1] === 150 ? "pointer" : "progress"}`
+                                    backgroundImage: `conic-gradient(yellow ${hoverDurationTwo / 1.5}%, transparent ${hoverDurationTwo}%`,
+                                    cursor: `${hoverDurationTwo === 150 ? "pointer" : "progress"}`
                                 }}>
-                                <Image src="/Public/Images/GameImages/YakuzaZero.jpg" fluid style={{ padding: '5px' }} />
+                                <Image src="/Public/Images/GameImages/YakuzaZero.jpg" fluid style={{
+                                    padding: '5px',
+                                    transition: 'transform 1s', transform: `scale(${hoverDurationTwo === 150 ? 1.1 : 1})`
+                                }} />
                             </div>
                         </Row>
                         <Row>
                             <Col style={{ paddingTop: '2%' }}>
 
                                 <Row >
-                                    <div className={`animated-border`} onMouseEnter={() => startTimer(2)} onMouseLeave={() => stopTimer(2)}
+                                    <div className={`animated-border`} onMouseEnter={() => startTimerThree()} onMouseLeave={() => stopTimerThree()}
                                         style={{
-                                            backgroundImage: `conic-gradient(purple ${hoverDuration[2] / 1.5}%, transparent ${hoverDuration[2]}%`,
-                                            cursor: `${hoverDuration[2] === 150 ? "pointer" : "progress"}`
+                                            backgroundImage: `conic-gradient(purple ${hoverDurationThree / 1.5}%, transparent ${hoverDurationThree}%`,
+                                            cursor: `${hoverDurationThree === 150 ? "pointer" : "progress"}`
                                         }}>
-                                        <Image src="/Public/Images/GameImages/SMTThree.jpg" fluid style={{ padding: '5px' }} />
+                                        <Image src="/Public/Images/GameImages/SMTThree.jpg" fluid style={{
+                                            padding: '5px',
+                                            transition: 'transform 1s', transform: `scale(${hoverDurationThree === 150 ? 1.15 : 1})`
+                                        }} />
                                     </div>
                                 </Row>
 
                                 <Row style={{ paddingTop: '4%' }}>
-                                    <div className={`animated-border`} onMouseEnter={() => startTimer(3)} onMouseLeave={() => stopTimer(3)}
+                                    <div className={`animated-border`} onMouseEnter={() => startTimerFour()} onMouseLeave={() => stopTimerFour()}
                                         style={{
-                                            backgroundImage: `conic-gradient(red ${hoverDuration[3] / 1.5}%, transparent ${hoverDuration[3]}%`,
-                                            cursor: `${hoverDuration[3] === 150 ? "pointer" : "progress"}`
+                                            backgroundImage: `conic-gradient(red ${hoverDurationFour / 1.5}%, transparent ${hoverDurationFour}%`,
+                                            cursor: `${hoverDurationFour === 150 ? "pointer" : "progress"}`
                                         }}>
-                                        <Image src="/Public/Images/GameImages/PersonaFive.jpg" fluid style={{ padding: '5px' }} />
+                                        <Image src="/Public/Images/GameImages/PersonaFive.jpg" fluid style={{
+                                            padding: '5px',
+                                            transition: 'transform 1s', transform: `scale(${hoverDurationFour === 150 ? 1.15 : 1})`
+                                        }} />
                                     </div>
                                 </Row>
 
                             </Col>
 
                             <Col style={{ paddingTop: '1%' }}>
-                                <div className={`animated-border`} onMouseEnter={() => startTimer(4)} onMouseLeave={() => stopTimer(4)}
+                                <div className={`animated-border`} onMouseEnter={() => startTimerFive()} onMouseLeave={() => stopTimerFive()}
                                     style={{
-                                        backgroundImage: `conic-gradient(blue ${hoverDuration[4] / 1.5}%, transparent ${hoverDuration[4]}%`,
-                                        cursor: `${hoverDuration[4] === 150 ? "pointer" : "progress"}`
+                                        backgroundImage: `conic-gradient(blue ${hoverDurationFive / 1.5}%, transparent ${hoverDurationFive}%`,
+                                        cursor: `${hoverDurationFive === 150 ? "pointer" : "progress"}`
                                     }}>
-                                    <Image src="/Public/Images/GameImages/MGR.jpg" fluid style={{ padding: '5px' }} />
+                                    <Image src="/Public/Images/GameImages/MGR.jpg" fluid style={{
+                                        padding: '5px',
+                                        transition: 'transform 1s', transform: `scale(${hoverDurationFive === 150 ? 1.1 : 1})`
+                                    }} />
                                 </div>
                             </Col>
 
