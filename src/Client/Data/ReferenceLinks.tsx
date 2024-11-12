cabcf89315fbbe2dd3200523e98eff81bd9b79b8
malamutes <hehexdroflmfao@gmail.com>
2024-11-12 17:43:07 +0800

    TopBar. PT1

--- src/Client/Data/ReferenceLinks.tsx
+++ src/Client/Data/ReferenceLinks.tsx
@@ -122,7 +122,61 @@ const ReferenceData: { [key: string]: { [key: string]: ReferenceDataInterface } 
     }
 }
 
-export { ReferenceData };
+const TopBarData: { [outerKey: string]: { [innerKey: string]: { title: string; image: string } } } = {
+
+    Place: {
+        Tokyo: { title: "Tokyo", image: '/Public/Images/EightCardImages/Tokyo.webp' },
+        Kyoto: { title: "Kyoto", image: '/Public/Images/EightCardImages/Kyoto.webp' },
+        Osaka: { title: "Osaka", image: '/Public/Images/EightCardImages/Osaka.webp' },
+        Hiroshima: { title: "Hiroshima", image: '/Public/Images/EightCardImages/Hiroshima.webp' },
+        Sapporo: { title: "Sapporo", image: '/Public/Images/EightCardImages/Sapporo.webp' },
+        Fukuoka: { title: "Fukuoka", image: '/Public/Images/EightCardImages/Fukuoka.webp' },
+        Nagoya: { title: "Nagoya", image: '/Public/Images/EightCardImages/Nagoya.webp' },
+        Nara: { title: "Nara", image: '/Public/Images/EightCardImages/Nara.webp' }
+    },
+    Food: {
+        Ramen: { title: "Ramen", image: "/Public/Images/FoodImages/Ramen.webp" },
+        Sushi: { title: "Sushi", image: "/Public/Images/FoodImages/Sushi.webp" },
+        Tempura: { title: "Tempura", image: "/Public/Images/FoodImages/Tempura.webp" },
+        Okonomiyaki: { title: "Okonomiyaki", image: "/Public/Images/FoodImages/Okonomiyaki.webp" },
+        Yakisoba: { title: "Yakisoba", image: "/Public/Images/FoodImages/Yakisoba.webp" },
+        Sashimi: { title: "Sashimi", image: "/Public/Images/FoodImages/Sashimi.webp" },
+        Mochi: { title: "Mochi", image: "/Public/Images/FoodImages/Mochi.webp" },
+        Udon: { title: "Udon", image: "/Public/Images/FoodImages/Udon.webp" }
+    },
+    Landmarks: {
+        Fujisan: { title: "Mount Fuji", image: "/Public/Images/LandmarkImages/MountFuji.webp" },
+        Sensoji: { title: "Sensō-ji Temple", image: "/Public/Images/LandmarkImages/SensoJiTemple.webp" },
+        FushimiInari: { title: "Fushimi Inari Shrine", image: "/Public/Images/LandmarkImages/FushimiInariShrine.webp" },
+        Kinkakuji: { title: "Golden Pavilion", image: "/Public/Images/LandmarkImages/KinkakuJi.webp" },
+        HiroshimaPeacePark: { title: "Hiroshima Peace Memorial Park", image: "/Public/Images/LandmarkImages/HiroshimaPeaceMemorialPark.webp" }
+    },
+    Entertainment: {
+        Anime: { title: "Anime", image: '/Public/Images/EntertainmentImages/anime.webp' },
+        JDrama: { title: "J-Drama", image: '/Public/Images/EntertainmentImages/AOTFront.webp' },
+        TraditionalArt: { title: "Japanese Traditional Art", image: '/Public/Images/EntertainmentImages/MonsterBack.webp' },
+        JMusic: { title: "J-Music", image: '/Public/Images/EntertainmentImages/MonsterBack.webp' },
+        Manga: { title: "Manga", image: '/Public/Images/EntertainmentImages/MonsterBack.webp' }
+    },
+    Game: {
+        Bloodborne: { title: "Bloodborne", image: "/Public/Images/GameImages/Bloodborne.webp" },
+        SMTThree: { title: "Shin Megami Tensei III: Nocturne", image: "/Public/Images/GameImages/SMTThree.webp" },
+        YakuzaZero: { title: "Yakuza Zero", image: "/Public/Images/GameImages/YakuzaZero.webp" },
+        PersonaFive: { title: "Persona 5", image: "/Public/Images/GameImages/PersonaFive.webp" },
+        MGR: { title: "Metal Gear Rising", image: "/Public/Images/GameImages/MGR.webp" }
+    },
+
+    Festival: {
+        SapporoSnowFestival: { title: "Sapporo Snow Festival", image: "/Public/Images/FestivalImages/SapporoSnowOne.webp" },
+        GionMatsuri: { title: "Gion Festival", image: "/Public/Images/FestivalImages/GionMatsuriOne.webp" },
+        AomoriNebutaMatsuri: { title: "Aomori Nebuta Festival", image: "/Public/Images/FestivalImages/AomoriNebutaOne.jpg" },
+        KandaMatsuri: { title: "Kanda Festival", image: "/Public/Images/FestivalImages/KandaMatsuriOne.webp" },
+        TakayamaMatsuri: { title: "Takayama Matsuri", image: "/Public/Images/FestivalImages/TakayamaMatsuriOne.webp" }
+    },
+
+};
+
+export { ReferenceData, TopBarData };
 
 //<a href="https://www.freepik.com/icon/lantern_7052524#fromView=search&page=1&position=13&uuid=251182de-2438-461c-bb91-2e5bd5fc5632">Icon by ToZ Icon</a>
 //<a href="https://www.freepik.com/icon/snowflake_1622133">Icon by Freepik</a>
--- src/Client/ReferenceBar/TopBar.tsx
+++ src/Client/ReferenceBar/TopBar.tsx
@@ -1,7 +1,41 @@
 import './ReferenceBar.css'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Offcanvas, Accordion, Image } from 'react-bootstrap'
+import { useState, useEffect } from 'react';
+import { TopBarData } from '../Data/ReferenceLinks';
 
 export default function TopBar() {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const [more1200px, setMore1200px] = useState(true);
+    const [currentHover, setCurrentHover] = useState("");
+
+    useEffect(() => {
+        function widthLess1200() {
+            if (window.innerWidth <= 1200) {
+                setMore1200px(false);
+            }
+            else if (window.innerWidth > 1200) {
+                setMore1200px(true);
+            }
+        };
+
+        widthLess1200();
+
+        window.addEventListener('resize', widthLess1200);
+
+        return () => {
+            window.removeEventListener('resize', widthLess1200);
+        };
+
+    }, []);
+
+
+
+    const TBD = TopBarData;
+
     return (
         <>
             <Container style={{
@@ -9,8 +43,82 @@ export default function TopBar() {
                 backgroundColor: 'red', height: '7.5%'
             }} fluid>
                 <Row>
-                    <Col xs={1} style={{ backgroundColor: 'blue' }}>
-                        HELLO WORLd
+                    <Col xs={1} >
+                        <button onClick={handleShow}>HELLO</button>
+
+                        {more1200px ? (<Offcanvas placement="top" show={show}
+                            style={{ height: '55vh' }}
+                            onHide={handleClose}>
+                            <Offcanvas.Header closeButton>
+                                <Offcanvas.Title>JAPAN</Offcanvas.Title>
+                            </Offcanvas.Header>
+                            <Offcanvas.Body>
+                                <Container style={{ maxWidth: '1824px' }}>
+                                    <Row>
+                                        <Col xs={8}>
+                                            <Row>
+                                                {Object.keys(TBD).map((topBarDataItem, index) => (
+                                                    <Col key={index} >
+                                                        <h6 style={{ marginBottom: '1cqw', marginLeft: '-0.25cqw' }}>{topBarDataItem}</h6>
+                                                        <Row >
+                                                            {Object.keys(TBD[topBarDataItem]).map((topBarDataInnerItem, innerIndex) => (
+                                                                <Col xs={12} style={{ marginBottom: '0.8cqw', marginLeft: '0cqw' }} key={innerIndex}>
+                                                                    <span className='TopBarText' onMouseEnter={() => setCurrentHover(TBD[topBarDataItem][topBarDataInnerItem].image)}
+                                                                        onMouseLeave={() => setCurrentHover("")}
+                                                                    >{TBD[topBarDataItem][topBarDataInnerItem].title}</span>
+                                                                </Col>
+                                                            ))}
+                                                        </Row>
+                                                    </Col>
+
+                                                ))}
+                                            </Row>
+
+                                        </Col>
+
+                                        <Col xs={4}>
+                                            <Image src={currentHover} style={{ maxWidth: '90%' }} />
+                                        </Col>
+
+                                    </Row>
+                                </Container>
+                            </Offcanvas.Body>
+                        </Offcanvas>)
+
+                            :
+
+                            (
+                                <Offcanvas placement="start" show={show}
+                                    onHide={handleClose}>
+                                    <Offcanvas.Header closeButton>
+                                        <Offcanvas.Title>JAPAN</Offcanvas.Title>
+                                    </Offcanvas.Header>
+                                    <Offcanvas.Body>
+                                        <Container style={{ maxWidth: '1824px' }}>
+                                            <Accordion>
+                                                {Object.keys(TBD).map((topBarDataItem, accordionIndex) => (
+                                                    <Accordion.Item eventKey={`${accordionIndex}`} key={accordionIndex} >
+                                                        <Accordion.Header>{topBarDataItem}</Accordion.Header>
+                                                        <Accordion.Body>
+                                                            <Col>
+                                                                {Object.keys(TBD[topBarDataItem]).map((topBarDataInnerItem, accordionInnerIndex) => (
+                                                                    <Row key={accordionInnerIndex}>
+                                                                        <Image style={{ cursor: 'pointer', marginBottom: '2.5cqw' }} src={TBD[topBarDataItem][topBarDataInnerItem].image} />
+                                                                    </Row>
+                                                                ))}
+                                                            </Col>
+
+                                                        </Accordion.Body>
+                                                    </Accordion.Item>
+                                                ))}
+
+                                            </Accordion>
+                                        </Container>
+                                    </Offcanvas.Body>
+                                </Offcanvas>
+                            )}
+
+
                     </Col>
 
                     <Col xs={7} style={{ backgroundColor: 'green' }}>
@@ -18,16 +126,16 @@ export default function TopBar() {
                     </Col>
 
                     <Col xs={2} style={{ backgroundColor: 'yellow' }}>
-
+                        ABOUT ME
                     </Col>
 
                     <Col xs={2} style={{ backgroundColor: 'purple' }}>
-
+                        MOTIVATION
                     </Col>
 
-                </Row>
+                </Row >
 
-            </Container>
+            </Container >
 
         </>
     )
